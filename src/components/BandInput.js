e03982284305e91e10edabc116d1dd7650119a90
Shami <shamitomita@gmail.com>
2022-02-11 21:30:23 -0500

    commit tree f4ebd5fbd5d95f52a7609390a055c7b997ee17bd

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,42 @@
 // Add BandInput component
+//form goes here
+//update this component state each time new character is typed in
+//only update reducx store when hitsubmit
+//dispatched action required to initiate should be set up in contatiner
+//passed down as the prop addBand to here
 import React, { Component } from 'react'
 
-class BandInput extends Component {
-  render() {
-    return(
-      <div>
-        Band Input
-      </div>
-    )
-  }
-}
 
-export default BandInput
+class BandInput extends Component {
+
+  state = {
+    name: ''
+  }
+  handleChange(event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    this.props.addBand(this.state)
+    this.setState({
+      name: '',
+    })
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(event) => this.handleChange(event)} />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+};
+
+export default (BandInput)
